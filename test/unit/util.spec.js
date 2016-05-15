6e65e04525714065ea222c1eed5c967a566df334
Alexandr Voronkin <admin@elasticweb.ru>
2016-05-15 23:41:53 +0300

    array support

--- test/unit/util.spec.js
+++ test/unit/util.spec.js
@@ -1,9 +1,90 @@
 var assert = require('assert')
 var util = require('../../lib/util')
 var getIds = util.getIds
+var get = util.get
 var zipObjectWith = util.zipObjectWith
 
 describe('util', function () {
+  describe('get', function () {
+    it("should return empty array if path doesn't found", function () {
+      var value = get({}, 'test test2')
+      assert.deepEqual([], value)
+    })
+
+    it('should support several paths', function () {
+      var value = get({
+        test: false,
+        test2: true
+      }, 'test test2')
+      assert.deepEqual([false, true], value)
+    })
+
+    it('should support deep path', function () {
+      var value = get({
+        test: {
+          test2: 2
+        }
+      }, 'test.test2')
+      assert.deepEqual([2], value)
+    })
+
+    it('should support several deep paths', function () {
+      var value = get({
+        test: {
+          test2: 2
+        },
+        foo: {
+          bar: 0
+        }
+      }, 'test.test2 foo.bar')
+      assert.deepEqual([2, 0], value)
+    })
+
+    it('should support arrays', function () {
+      var value = get({
+        test: [{
+          test2: 2
+        }, {
+          test2: 2
+        }, {
+          test2: 4
+        }]
+      }, 'test.test2 foo.bar')
+      assert.deepEqual([2, 2, 4], value)
+    })
+
+    it('should support arrays', function () {
+      var value = get({
+        test: [{
+          test2: {
+            foo: [
+              {
+                bar: 2
+              }
+            ]
+          }
+        }, {
+          test2: {
+            foo: [
+              {
+                bar: 2
+              }
+            ]
+          }
+        }, {
+          test2: {
+            foo: [
+              {
+                bar: 4
+              }
+            ]
+          }
+        }]
+      }, 'test.test2.foo.bar')
+      assert.deepEqual([2, 2, 4], value)
+    })
+  })
+
   describe('getIds', function () {
     it("should return empty array if path doesn't found", function () {
       var ids = getIds({}, 'test')
