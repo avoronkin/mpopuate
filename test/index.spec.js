0ad06a299397f8dd2c8792b253dc584ed5deff96
Alexandr Voronkin <admin@elasticweb.ru>
2018-04-08 19:06:50 +0300

    projection

--- test/index.spec.js
+++ test/index.spec.js
@@ -20,7 +20,7 @@ describe('mpopulate', function () {
         ])
     })
 
-    it('should populate', async () => {
+    it('should populate #1', async () => {
         const posts = await db.collection('posts').find().toArray()
 
         await mpopulate([
@@ -39,8 +39,8 @@ describe('mpopulate', function () {
                 db
             }
         })(posts, {
-            'comments.user.name': 1,
-            'autor.name': 1
+            'comments.user':{'name': 1},
+            author: {name: 1}
         })
 
         assert.deepEqual(posts[0], {
@@ -82,6 +82,87 @@ describe('mpopulate', function () {
                         _id: id(4),
                         name: 'user4'
                     }
+                }
+            ]
+        })
+    })
+
+
+    it('should populate #2', async () => {
+        const posts = await db.collection('posts').find().toArray()
+
+        await mpopulate([
+            {
+                localField: 'author comments.user',
+                from: 'users',
+                foreignField: '_id',
+            },
+            {
+                localField: 'comments',
+                from: 'comments',
+                foreignField: '_id',
+            }
+        ],{
+            mongodb: {
+                db
+            }
+        })(posts, {
+            author: {name: 1}
+        })
+
+        assert.deepEqual(posts[0], {
+            _id: id(1),
+            title: 'title1',
+            author: {
+                _id: id(1),
+                name: 'user1'
+            },
+            comments: [ id(1), id(2), id(3), id(4) ]
+        })
+    })
+
+    it('should populate #3', async () => {
+        const posts = await db.collection('posts').find().toArray()
+
+        await mpopulate([
+            {
+                localField: 'author comments.user',
+                from: 'users',
+                foreignField: '_id',
+            },
+            {
+                localField: 'comments',
+                from: 'comments',
+                foreignField: '_id',
+            }
+        ],{
+            mongodb: {
+                db
+            }
+        })(posts, {
+            comments: {text: 1}
+        })
+
+        assert.deepEqual(posts[0], {
+            _id: id(1),
+            title: 'title1',
+            author: id(1),
+            comments: [
+                {
+                    _id: id(1),
+                    text: 'text1',
+                },
+                {
+                    _id: id(2),
+                    text: 'text2',
+                },
+                {
+                    _id: id(3),
+                    text: 'text3',
+                },
+                {
+                    _id: id(4),
+                    text: 'text4',
                 }
             ]
         })
